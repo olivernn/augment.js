18cb97d4e4adc6da59d438602e44888cc5bc4d3b
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-03-29 15:32:26 +0100

    reference mozilla in the header

--- src/header.js
+++ src/header.js
@@ -1,4 +1,5 @@
 // augment.js JavaScript 1.8.5 methods for all, version: @VERSION
+// using snippets from Mozilla - https://developer.mozilla.org/en/JavaScript
 // (c) 2011 Oliver Nightingale
 //
 //  Released under MIT license.
