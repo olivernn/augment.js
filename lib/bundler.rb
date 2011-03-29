c617591d4befb08341f3f4637aab1b7425a2a0ef
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-03-29 14:12:14 +0100

    more stuff

---
+++ lib/bundler.rb
@@ -0,0 +1,62 @@
+require 'fileutils'
+
+require 'rubygems'
+require 'closure-compiler'
+require 'fewer'
+
+class Bundler
+  DIST_DIR = File.expand_path('../../dist', __FILE__)
+  SRC_DIR = File.expand_path('../../src', __FILE__)
+
+  class << self
+    def bundle!
+      FileUtils.mkdir_p(DIST_DIR)
+
+      write "#{DIST_DIR}/augment-#{version}.js" do
+        Fewer::Engines::Js.new(SRC_DIR, files).read
+      end
+
+      write "#{DIST_DIR}/augment-#{version}.min.js" do
+        Fewer::Engines::Js.new(SRC_DIR, files, :min => true).read
+      end
+    end
+
+    def bundled
+      Fewer::Engines::Js.new(SRC_DIR, files).read
+    end
+
+    private
+      def files
+        @files ||= %w(
+          augment.array.every
+          augment.array.filter
+          augment.array.forEach
+          augment.array.indexOf
+          augment.array.isArray
+          augment.array.lastIndexOf
+          augment.array.map
+          augment.array.reduce
+          augment.array.reduceRight
+          augment.array.some
+          augment.object.keys
+        )
+      end
+
+      def header
+        @header ||= File.read(File.join(SRC_DIR, 'header.js'))
+      end
+
+      def version
+        @version ||= File.read('VERSION').strip
+      end
+
+      def write(path, &block)
+        puts "Generating #{path}"
+
+        File.open(path, 'w') do |f|
+          f.write header.gsub('@VERSION', version)
+          f.write yield.gsub('@VERSION', version)
+        end
+      end
+  end
+end
