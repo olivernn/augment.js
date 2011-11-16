8cd70ece8e4fb4ad8583f24a14a85ec849ff3687
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-11-16 10:27:30 +0000

    simpler implementation of getPrototypeOf

--- src/augment.object.getPrototypeOf.js
+++ src/augment.object.getPrototypeOf.js
@@ -3,45 +3,18 @@
   var ensureIsObject = function (param) {
     if (param !== Object(param)) throw new TypeError('Object.getPrototypeOf called on non-object');
   }
-  
-  if (true || !Object.getPrototypeOf) {
+
+  if (!Object.getPrototypeOf) {
     if (typeof "test".__proto__ === "object") {
       Object.getPrototypeOf = function (obj) {
         ensureIsObject(obj)
-        return obj.__proto__;
-      };
+        return obj.__proto__
+      }
     } else {
       Object.getPrototypeOf = function (obj) {
         ensureIsObject(obj)
-        alert("bar")
-        var constructor = obj.constructor,
-        oldConstructor;
-        if (Object.prototype.hasOwnProperty.call(obj, "constructor")) {
-          oldConstructor = constructor;
-          if (!(delete obj.constructor)) // reset constructor
-            return null; // can't delete obj.constructor, return null
-          constructor = obj.constructor; // get real constructor
-          obj.constructor = oldConstructor; // restore constructor
-        }
-        return constructor ? constructor.prototype : null; // needed for IE
-      };
-    }
-    
-
-    // var ensureIsObject = function (param) {
-    //   if (param !== Object(param)) throw new TypeError('Object.getPrototypeOf called on non-object');
-    // }
-    // 
-    // if (typeof "test".__proto__ === "object") {
-    //   Object.getPrototypeOf = function (obj) {
-    //     ensureIsObject(obj)
-    //     return obj.__proto__
-    //   }
-    // } else {
-    //   Object.getPrototypeOf = function (obj) {
-    //     ensureIsObject(obj)
-    //     return obj.constructor.prototype
-    //   }
-    // }
-  }
+        return obj.constructor.prototype
+      }
+    };
+  };
 })();
--- test/getPrototypeOf_test.js
+++ test/getPrototypeOf_test.js
@@ -3,13 +3,22 @@ module("getPrototypeOf")
 test("returns the prototype of an object", function () {
   var Foo = function () {}
   Foo.prototype = {
-    bar: true
+    bar: true,
+    constructor: Foo
   }
 
   var foo = new Foo ()
 
   same(Object.getPrototypeOf(foo), Foo.prototype, "should return the prototype of an object")
-  
+})
+
+test("returns the prototype of an object with prototype prototypes set individually", function () {
+  var Foo = function () {}
+  Foo.prototype.bar = true
+
+  var foo = new Foo
+
+  same(Object.getPrototypeOf(foo), Foo.prototype, "should return the prototype of an object")
 })
 
 test("throws a TypeError if called on a non object", function () {
