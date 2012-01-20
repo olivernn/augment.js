b44f180a07c38d1a89cfc380d7208fa073ab2b3e
Oliver Nightingale <olivernightingale@Olivers-MacBook-Pro.local>
2012-01-20 15:12:46 +0000

    handle the don't enum bug

--- test/keys_test.js
+++ test/keys_test.js
@@ -40,3 +40,62 @@ test("throws an error when called on a non object", function () {
     Object.keys("no dice")
   }, TypeError)
 })
+
+test("watch out for the dont enum bug", function () {
+  var obj = {
+      constructor: function () {
+        return 0
+      },
+
+      toString: function () {
+        return 1
+      },
+
+      valueOf: function () {
+        return 2
+      },
+
+      toLocaleString: function () {
+        return 3
+      },
+
+      prototype: function () {
+        return 4
+      },
+
+      isPrototypeOf: function () {
+        return 5
+      },
+
+      propertyIsEnumerable: function () {
+        return 6
+      },
+
+      hasOwnProperty: function () {
+        return 7
+      },
+
+      length: function () {
+        return 8
+      },
+
+      unique: function () {
+        return 9
+      }
+    }
+
+  var expected = [
+    'constructor',
+    'toString',
+    'valueOf',
+    'toLocaleString',
+    'prototype',
+    'isPrototypeOf',
+    'propertyIsEnumerable',
+    'hasOwnProperty',
+    'length',
+    'unique'
+  ]
+
+  same(expected.sort(), Object.keys(obj).sort(), 'should have all the object keys from the object')
+})
