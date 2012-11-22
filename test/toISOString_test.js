c711fe291c640b42293f20281b24732d71b673b2
Oliver Nightingale <oliver.nightingale1@gmail.com>
2012-11-22 13:02:07 +0000

    use utc dates so tests pass in all timezone

--- test/toISOString_test.js
+++ test/toISOString_test.js
@@ -1,8 +1,18 @@
 module("Date.prototype.toISOString")
 
 // date has been stubbed with sinon
-var stubbedDate = function () {
-  return new Date ()
+var stubbedDate = function (fullYear) {
+  var d = new Date (0)
+
+  d.setUTCFullYear(fullYear || 1970)
+  d.setUTCMonth(0)
+  d.setUTCDate(1)
+  d.setUTCHours(0)
+  d.setUTCMinutes(0)
+  d.setUTCSeconds(0)
+  d.setUTCMilliseconds(0)
+
+  return d
 }
 
 test("converts a date to an ISO string", function () {
@@ -14,7 +24,7 @@ test("converts a date to an ISO string", function () {
 test("handling months correctly", function () {
   var date = stubbedDate()
 
-  date.setMonth(1)
+  date.setUTCMonth(1)
   equal('1970-02-01T00:00:00.000Z', date.toISOString(), "should take care of javascripts crazy months")
 })
 
@@ -29,11 +39,11 @@ test("handling padding of milliseconds correctly", function () {
 })
 
 test("year is greater than 9999", function () {
-  var date = new Date(10000, 0, 1)
+  var date = stubbedDate(10000)
   equal('+010000-01-01T00:00:00.000Z', date.toISOString(), "should correctly handle dates with years greater than 9999")
 })
 
 test("year is less than 0", function () {
-  var date = new Date (-1, 0, 1)
+  var date = stubbedDate(-1)
   equal('-000001-01-01T00:00:00.000Z', date.toISOString(), "should correctly handle dates with years less than 9999")
 })
